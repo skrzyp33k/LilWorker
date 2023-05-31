b43c576164a1070ada69d27f1568a57a5d8a32d7
Kacper Skrzypek <skrzyp33k@proton.me>
2023-05-31 12:36:04 +0200

    implemented file sending with tcp - not testet yet!

--- LilWorker/Essentials/TCPSocket.cs
+++ LilWorker/Essentials/TCPSocket.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,28 @@ namespace LilWorker.Essentials
 {
     internal class TCPSocket
     {
+        public static void SendFile(string ipAddress, int port, string filePath)
+        {
+            byte[] fileData = File.ReadAllBytes(filePath);
+
+            try
+            {
+                using (TcpClient client = new TcpClient(ipAddress, port))
+                {
+                    NetworkStream stream = client.GetStream();
+
+                    stream.Write(fileData, 0, fileData.Length);
+
+                    byte[] endMarker = Encoding.ASCII.GetBytes("LilWorkerStartJob");
+                    stream.Write(endMarker, 0, endMarker.Length);
+
+                    Console.WriteLine("File transfer successfully completed!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred while uploading the file: " + ex.Message);
+            }
+        }
     }
 }
